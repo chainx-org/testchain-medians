296a5ace85a66cada49694bf0bf5f8607bf96afe
hacpy <hacpy.lbx@gmail.com>
2022-07-20 11:55:24 +0800

    Add xbtc median

--- src/median.sol
+++ src/median.sol
@@ -30,6 +30,17 @@ contract MedianPCXUSD is Median {
     }
 }
 
+contract MedianPCXUSD is Median {
+    bytes32 public constant wat = "XBTCUSD";
+
+    function recover(uint256 val_, uint256 age_, uint8 v, bytes32 r, bytes32 s) internal pure returns (address) {
+        return ecrecover(
+            keccak256(abi.encodePacked("\x19Ethereum Signed Message:\n32", keccak256(abi.encodePacked(val_, age_, wat)))),
+            v, r, s
+        );
+    }
+}
+
 contract MedianKSXUSD is Median {
     bytes32 public constant wat = "KSXUSD";
 
